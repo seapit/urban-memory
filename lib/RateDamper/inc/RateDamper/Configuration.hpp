1765fb357d13f22126ea398baf0b95cf60561e8b
Carlo Pitruzzello <carlo.pitruzzello+git@gmail.com>
2026-08-23 16:35:00 -0400

    Move configuration base to its own header file, keep derived values in RateDamper Maximse use of constexpr so we can get some constants decided at compile time
    Remove unnecessary wasteful vars

---
+++ lib/RateDamper/inc/RateDamper/Configuration.hpp
@@ -0,0 +1,74 @@
+/**
+ * \file Configuration.hpp
+ * \brief A brief description of what this file is.
+ */
+
+#pragma once
+/// INCLUDES
+#include <cstdint>
+
+#include <numbers>
+/// CMAKE INCLUDES
+// #include "RateDamper/version.h"
+
+/// USER INCLUDES
+
+/// NAMESPACE
+
+/// DEFINES
+
+/// CODE
+
+// this is ugly but i don't know if there's a ready made library for conversions
+static inline constexpr std::size_t seconds_per_minute{
+    60}; // units of sec/minute
+static inline constexpr double seconds_to_minute{
+    1.0 / seconds_per_minute}; // units of minute/sec
+
+static inline constexpr double pi{std::numbers::pi};
+
+struct RateDamperConfiguration {
+  // Limits
+  double maximumAngularMomentum_N_m_s{0};
+  // defer to floor - data sheet is odd and indicates >= 1 N*m,
+  // must be missing something since this shouldn't be a max
+  double outputTorque_Floor_N_m{0};
+  double maximumAngularRate_rev_min{0};
+
+  double nominalVoltage_V{0.0};
+  double nominalVoltageVariation_V{0};
+  double steadyStatePowerConsumption_W{0.0};
+  double maximumPowerConsumption_W{0.0};
+
+  // Control
+  // damping factor has torque = k*w
+  // (math is probably off here)
+  // any value >0 would converge? I think?
+  // lets go with a damping factor between [0.0, 1.0]
+  // https://ntrs.nasa.gov/api/citations/19980236616/downloads/19980236616.pdf
+  // page 13 #3 indicates the use .2 or .5, initial assumption is correct
+
+  // torque = Nm, w = rad/sec
+  // must have units of N * m * sec/rad
+  //                              N m / (rad/sec) = N m s / rad
+  double dampingGain_N_m_s_per_rad{0.0};
+
+  // Physical properties, not needed but lets do a complete job
+  double mass_kg{0.0};
+  double diameter_mm{0.0};
+  double height_mm{0.0};
+
+  // helper function
+  constexpr double convertAngularRate_rpm_to_rad_sec() const noexcept {
+    return ((maximumAngularRate_rev_min * seconds_to_minute) * (2 * pi));
+  };
+
+  // Lets add a sanity check
+  // hopefully brownie points
+  //  W = 1 N*m/s ,
+  // P_max = torque * AngularVel_max
+  constexpr bool isRealisticWheel() const {
+    return (maximumPowerConsumption_W >=
+            (outputTorque_Floor_N_m * convertAngularRate_rpm_to_rad_sec()));
+  };
+};
--- lib/RateDamper/inc/RateDamper/RateDamper.hpp
+++ lib/RateDamper/inc/RateDamper/RateDamper.hpp
@@ -9,32 +9,17 @@
 // #include <cstdio>
 #include <algorithm>
 #include <cstdint>
-#include <numbers>
 
 /// CMAKE INCLUDES
 // #include "RateDamper/version.h"
 
 /// USER INCLUDES
 #include "Common/HelperMacros.hpp"
+#include "RateDamper/Configuration.hpp"
 
 /// NAMESPACE
 
 /// DEFINES
-
-/// CODE
-
-// this is ugly but i don't know if there's a ready made library for conversions
-static inline constexpr std::size_t seconds_per_minute{
-    60}; // units of sec/minute
-static inline constexpr double seconds_to_minute{
-    1.0 / seconds_per_minute}; // units of minute/sec
-
-static inline constexpr double mm_per_m{1000};          // units of mm/m
-static inline constexpr double mm_to_m{1.0 / mm_per_m}; // units of m/mm
-
-static inline constexpr double pi{std::numbers::pi};
-
-// using this reaction wheel
 // https://satsearch.co/products/oce-technology-rw1000-reaction-wheel
 // maximum angular momentum 11 N m s
 // maximum output torque // ≥ 1 N m
@@ -46,49 +31,48 @@ static inline constexpr double pi{std::numbers::pi};
 // diameter 337 mm
 // height 121 mm
 // mass  ≤10 kg
-struct RateDamperConfiguration {
-  // Limits
-  static inline constexpr double maximumAngularMomentum_N_m_s{11};
-  // defer to floor - data sheet is odd and indicates >= 1 N*m,
-  // must be missing something since this shouldn't be a max
-  static inline constexpr double outputTorque_Floor_N_m{1.0};
-  static inline constexpr double maximumAngularRate_rev_min{1200};
-  static inline constexpr double maximumAngularRate_rad_sec{
-      // Rev/min * min/sec *rad/rev = Rev/sec * rad/rev = rad/sec
-      (maximumAngularRate_rev_min * seconds_to_minute) * (2 * pi)};
 
-  static inline constexpr double nominalVoltage_V{28.0};
-  static inline constexpr double nominalVoltageVariation_V{3.0};
-  static inline constexpr double steadyStatePowerConsumption_W{15.0};
-  static inline constexpr double maximumPowerConsumption_W{160.0};
+inline constexpr RateDamperConfiguration RW1000Configuration{
+    // Limits
+    .maximumAngularMomentum_N_m_s = 11,
+    // defer to floor - data sheet is odd and indicates >= 1 N*m,
+    // must be missing something since this shouldn't be a max
+    .outputTorque_Floor_N_m = 1.0,
+    .maximumAngularRate_rev_min = 1200,
 
-  // Control
-  // damping factor has torque = k*w
-  // (math is probably off here)
-  // any value >0 would converge? I think?
-  // lets go with a damping factor between [0.0, 1.0]
-  // https://ntrs.nasa.gov/api/citations/19980236616/downloads/19980236616.pdf
-  // page 13 #3 indicates the use .2 or .5, initial assumption is correct
+    .nominalVoltage_V = 28.0,
+    .nominalVoltageVariation_V = 3.0,
 
-  // torque = Nm, w = rad/sec
-  // must have units of N * m * sec/rad
-  //                              N m / (rad/sec) = N m s / rad
-  static inline constexpr double dampingGain_N_m_s_per_rad{0.5};
+    .steadyStatePowerConsumption_W = 15.0,
+    .maximumPowerConsumption_W = 160.0,
 
-  // Physical properties, not needed but lets do a complete job
-  static inline constexpr double mass_kg{10};
-  static inline constexpr double diameter_mm{337};
-  static inline constexpr double height_mm{121};
+    // Control
+    // damping factor has torque = k*w
+    // (math is probably off here)
+    // any value >0 would converge? I think?
+    // lets go with a damping factor between [0.0, 1.0]
+    // https://ntrs.nasa.gov/api/citations/19980236616/downloads/19980236616.pdf
+    // page 13 #3 indicates the use .2 or .5, initial assumption is correct
 
-  // Lets add a sanity check
-  // hopefully brownie points
-  //  W = 1 N*m/s ,
-  // P_max = torque * AngularVel_max
-  static_assert(maximumPowerConsumption_W >=
-                    (outputTorque_Floor_N_m * maximumAngularRate_rad_sec),
-                "configuration likely incorrect, not physically possible "
-                "as a device");
+    // torque = Nm, w = rad/sec
+    // must have units of N * m * sec/rad
+    //                              N m / (rad/sec) = N m s / rad
+    .dampingGain_N_m_s_per_rad = 0.5,
+
+    // Physical properties, not needed but lets do a complete job
+    .mass_kg = 10,
+    .diameter_mm = 337,
+    .height_mm = 121,
+
+    // Lets add a sanity check
+    // hopefully brownie points
+    //  W = 1 N*m/s ,
+    // P_max = torque * AngularVel_max
 };
+static_assert(RW1000Configuration.isRealisticWheel(),
+              "configuration likely incorrect");
+
+/// CODE
 
 /**
  * \class RateDamper
@@ -136,9 +120,8 @@ public:
   };
 
   double boundedAngularRateValidation(double rhsInput) noexcept {
-    aRetVal =
-        std::clamp(rhsInput, (0 - damperConfig.maximumAngularRate_rad_sec),
-                   damperConfig.maximumAngularRate_rad_sec);
+    double aMaximumRate = damperConfig.convertAngularRate_rpm_to_rad_sec();
+    aRetVal = std::clamp(rhsInput, (0 - aMaximumRate), aMaximumRate);
     return aReturn;
   }
   // protected:
