2c0164742546530c9a72720397b3f6c01a192a04
Carlo Pitruzzello <carlo.pitruzzello+git@gmail.com>
2026-08-23 21:17:35 -0400

    First pass on Health Monitor

---
+++ lib/HMS/inc/HMS/HealthMonitor.hpp
@@ -0,0 +1,61 @@
+/**
+ * \file HealthMonitor.hpp
+ * \brief A brief description of what this file is.
+ */
+
+#pragma once
+/// INCLUDES
+#include <array>
+
+/// CMAKE INCLUDES
+// #include "HMS/version.h"
+
+/// USER INCLUDES
+#include "HMS/SensorDefinitions.hpp"
+#include "HMS/TelemetryDB.hpp"
+
+/// NAMESPACE
+
+/// DEFINES
+
+/// CODE
+/**
+ * \class HealthMonitor
+ */
+class HealthMonitor {
+public:
+  /**
+   * \brief Construct a new HealthMonitor object
+   *
+   */
+  HealthMonitor(healthMonitorConfiguration &rhsConfiguration,
+                const TelemetryDB &rhsDB)
+      : stored_Configuration(rhsConfiguration), telemetryDatabase(rhsDB) {};
+
+  void step(std::chrono::milliseconds rhsElapsedTime) noexcept;
+
+  void flagPrimedSensor(TelemetryIds rhsId) const noexcept;
+
+  void evaluateRateOfChange() const noexcept;
+
+  // protected:
+
+private:
+  healthMonitorConfiguration stored_Configuration;
+  std::array<sensorState, healthMonitorConfiguration::requiredNumberofChannels>
+      sensorState;
+
+  std::array<sensorConfiguration,
+             healthMonitorConfiguration::requiredNumberofChannels>
+      sensorConfigurations;
+
+  // sufficient for a rate of change calculation
+  // can
+  std::array<tmSample, healthMonitorConfiguration::requiredNumberofChannels>
+      previousSample{};
+
+  std::array<std::size_t, healthMonitorConfiguration::requiredNumberofChannels>
+      oldestSampleIndex{};
+
+  const TelemetryDB &telemetryDatabase;
+};
