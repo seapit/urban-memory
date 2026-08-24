833381140b90ae1989a30ee361c5cb2622c2b1bd
Carlo Pitruzzello <carlo.pitruzzello+git@gmail.com>
2026-08-23 23:09:36 -0400

    comment out placeholders

--- apps/OBCDemo/src/main.cpp
+++ apps/OBCDemo/src/main.cpp
@@ -8,8 +8,6 @@
 // #include <unistd.h>
 
 /// INCLUDES
-// #include <csignal>
-// #include <cstdio>
 #include <chrono>
 #include <cstdint>
 
@@ -30,23 +28,23 @@ int main() {
   // we'll use steps to test our system - 1000 Hz = 1ms tick rate
   // most ambitious linux tick rate
   // not quite sure how
-  static constexpr std::uint16_t systemTickRate_Hz{1000};
+  // static constexpr std::uint16_t systemTickRate_Hz{1000};
 
   // duration of sim
-  static constexpr std::chrono::seconds simulationDuration{30};
+  // static constexpr std::chrono::seconds simulationDuration{30};
   // number of steps
   //                                            Convert from seconds -> msecs ->
   //                                            get the count
-  static constexpr std::uint16_t numberOfSteps{
-      std::chrono::duration_cast<std::chrono::milliseconds>(simulationDuration)
-          .count()};
+  // static constexpr std::uint16_t numberOfSteps{
+  //     std::chrono::duration_cast<std::chrono::milliseconds>(simulationDuration)
+  //         .count()};
 
-  static constexpr std::uint8_t sensor_A_PollRate_Hz{10};
-  static constexpr std::uint8_t sensor_B_PollRate_Hz{50};
-  static constexpr std::uint8_t sensor_C_PollRate_Hz{1};
+  // static constexpr std::uint8_t sensor_A_PollRate_Hz{10};
+  // static constexpr std::uint8_t sensor_B_PollRate_Hz{50};
+  // static constexpr std::uint8_t sensor_C_PollRate_Hz{1};
 
   while (true) {
-    sleep(10);
+    // sleep(10);
   }
   return 0;
 }
