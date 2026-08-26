31cffd6c1b34db3c27d126a2d1707dcd5e6d116a
Carlo Pitruzzello <carlo.pitruzzello+git@gmail.com>
2026-08-25 21:54:16 -0400

    move tm back into db, update unit tests and cmakes accordinly

--- lib/AOCS/RateDamper/test/test_RateDamper.cpp
+++ lib/AOCS/RateDamper/test/test_RateDamper.cpp
@@ -1,5 +1,6 @@
 #include "RateDamper/RateDamper.hpp"
 #include "RateDamper/Wheels/RW1000.hpp"
+
 #include <gtest/gtest.h>
 // Define a test fixture class
 class RateDamperTest : public ::testing::Test { // NOSONAR
@@ -24,12 +25,37 @@ protected:
     // Code here will be called immediately after each test (right
     // before the destructor).
   }
-  RateDamper(RW1000Configuration);
+  RateDamper aDamper{RW1000Configuration};
 };
 
 // Test cases using the test fixture
 TEST_F(RateDamperTest, InitProbably) {}
 
-TEST_F(RateDamperTest, Bounds_Torque) { auto aLowValue = 0; }
-TEST_F(RateDamperTest, Bounds_Voltage) {}
-TEST_F(RateDamperTest, Bounds_AngularRate) {}
+TEST_F(RateDamperTest, boundedTests) {
+  double aLimitCheckTorqueClamp =
+      (RW1000Configuration.outputTorque_Floor_N_m + 100.0);
+
+  double aVoltageCheck = (RW1000Configuration.nominalVoltageVariation_V + 0.1);
+
+  EXPECT_EQ(aDamper.boundedTorqueValidation(aLimitCheckTorqueClamp),
+            RW1000Configuration.outputTorque_Floor_N_m);
+  EXPECT_EQ(aDamper.boundedTorqueValidation((0 - aLimitCheckTorqueClamp)),
+            (0 - RW1000Configuration.outputTorque_Floor_N_m));
+
+  EXPECT_EQ(aDamper.boundedVoltageValidation(
+                RW1000Configuration.nominalVoltage_V + aVoltageCheck),
+            (RW1000Configuration.nominalVoltage_V +
+             RW1000Configuration.nominalVoltageVariation_V));
+  EXPECT_EQ(aDamper.boundedVoltageValidation(
+                (RW1000Configuration.nominalVoltage_V - aVoltageCheck)),
+            (RW1000Configuration.nominalVoltage_V -
+             RW1000Configuration.nominalVoltageVariation_V));
+
+  double amaximumInputRate = RW1000Configuration.outputTorque_Floor_N_m /
+                             RW1000Configuration.dampingGain_N_m_s_per_rad;
+
+  EXPECT_EQ(aDamper.boundedAngularRateValidation(amaximumInputRate + 1),
+            amaximumInputRate);
+  EXPECT_EQ(aDamper.boundedAngularRateValidation((0 - amaximumInputRate)),
+            amaximumInputRate);
+}
