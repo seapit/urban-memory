d093b5afb62eaa40845dbbc530021de898858d3b
Carlo Pitruzzello <carlo.pitruzzello+git@gmail.com>
2026-08-26 23:43:46 -0400

    Move implementations to cpp

--- lib/FDIR/inc/FDIR/FDIR.hpp
+++ lib/FDIR/inc/FDIR/FDIR.hpp
@@ -46,58 +46,18 @@ public:
   // only one alarm can be actioned at a time, we are simulating an RTOS
   // tasks are bounded and shouldn't slip
   // decide which alarms are ok, THEN action the first one we can.
-  void
-  step([[maybe_unused]] std::chrono::milliseconds rhsElapsedTime) noexcept {
-    alarmEntry aAlarm;
-    // loop will keep executing as long as there are things in the queue
-    // we should ensure these are valid before we address them
-    while (alarmBuffer.try_pop(aAlarm)) {
-      checkAlarmValidity(aAlarm);
-    }
-  }
+  void tick(std::chrono::milliseconds rhsElapsedTime) noexcept;
 
   void actionAlarm() {}
   // protected:
 
 private:
-  void checkAlarmValidity(const alarmEntry &rhsEntry) {
+  void checkAlarmValidity(const alarmEntry &rhsEntry) noexcept;
 
-    // filter out TMs we cant action
-    // ensure it's valid
-    if (isValid(rhsEntry) && isEligible(rhsEntry.id)) {
-      actionableAlarm = true;
-    }
-    // check if we've corrected
-    if (isValid(rhsEntry) && (rhsEntry.cause == Alarm::empty)) {
-      actionableAlarm = false;
-    }
-  }
+  bool isEligible(TelemetryIds lhsId) const noexcept;
 
-  bool isEligible(TelemetryIds lhsId) {
-    aRetVal{false};
-    for (const auto &[workableId, _] : faultConfiguration.eligibleTelemetries) {
-      if (lhsId == workableId) {
-        {
-          aReturn = true;
-          break;
-        }
-      }
-    }
-    return aReturn;
-  }
-
-  bool isValid(const alarmEntry &rhsEntry) {
-    bool aReturn = false;
-
-    if (static_cast<bool>(rhsEntry.cause & Alarm::rateofchange) ||
-        static_cast<bool>(rhsEntry.cause & Alarm::too_high) ||
-        static_cast<bool>(rhsEntry.cause & Alarm::too_low)) {
-      aReturn = true;
-    }
-    return aReturn;
-  }
-
-  [[maybe_unused]] bool actionableAlarm{false};
+  bool isValid(const alarmEntry &rhsEntry) const noexcept;
+  bool actionableAlarm{false};
   [[maybe_unused]] bool actionedAlarm{false};
   [[maybe_unused]] bool continueActioning{false};
 
